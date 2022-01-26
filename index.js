d62143ea4584fb06cd7e6efdb1a7805577d69bdd
fahimahammed <fahimahammed.cse@gmail.com>
2022-01-27 02:16:12 +0600

    first npm package

---
+++ index.js
@@ -0,0 +1,5 @@
+function isVowel(char){
+    return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U';
+}
+
+module.exports = isVowel
